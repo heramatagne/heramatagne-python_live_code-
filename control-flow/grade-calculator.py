d4f256ba34eb23cf7c32b955b1b7d8c974e57485
hera-admin hera-admin+1-at-101171522119 <hera-admin hera-admin+1-at-101171522119>
2022-11-01 22:55:32 -0700

    This commit contains 2 main programs. First to check if the input number is odd or even. Second to calculate the average grade of student and print grade from A to E.

--- control-flow/grade-calculator.py
+++ control-flow/grade-calculator.py
@@ -4,8 +4,14 @@ mathematics = float(input("grade between 0-100: "))
 physics = float(input("grade a number between 0-100: "))
 chemestry = float(input("grade a number between 0-100: "))
 english = float(input("grade a number between 0-100: "))
+
+# Calculation the average grade of student.
 average_grade = (history + mathematics + physics + chemestry + english)/5
+
+#print student's average grade.
 print(average_grade)
+
+# Classified average grade from A-E
 if average_grade > 90:
     print(f"{average_grade} is A")
 elif average_grade > 80:
