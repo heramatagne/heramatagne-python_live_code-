de887c0208f0bd1d31a4f7323493ac6aed87b1af
hera-admin hera-admin+1-at-101171522119 <hera-admin hera-admin+1-at-101171522119>
2022-11-18 16:09:25 -0800

    This program creates an English to French Translation Dictionary and an English to Bulgarian Dictionary.

---
+++ container-dictionary/translation-dictionary.py
@@ -0,0 +1,42 @@
+#This program creates an English to French and English to Bulgarian translation dictionary.
+
+#This dictionary translate a  word from English to French.
+english_to_french_dictionary = {
+    "Family" : "Famille",
+    "Parent" : "Parent",
+    "Father" : "Pere",
+    "Mother" : "Mere",
+    "child" : "Enfant",
+    "Son" : "Fils",
+    "Daugther" : "Fille",
+    "Brother" : "Frere",
+    "Sister" : "Soeur",
+    "Safou Plum" : "Prune/Safou"
+}
+
+#This dictionary translate a  word from English to bulgarian.
+english_to_bulgarian_dictionary = {
+    "Love" : "любов",
+    "Grandparents" : "Дядо и баба",
+    "Dad" : "татко",
+    "Mum" : "мама",
+    "kid" : "хлапе",
+    "Boy" : "момче",
+    "Girl" : "момиче",
+    "Uncle" : "чичо",
+    "Aunt" : "леля",
+    "Plum" : "слива"
+}
+my_word = input("Enter your word \n")
+
+#English to French translation
+translation = english_to_french_dictionary.get(my_word)
+#English to Bulgarian translation
+translation2= english_to_bulgarian_dictionary.get(my_word)
+
+if my_word in english_to_french_dictionary:
+    print("{} means {}".format (my_word, translation))
+elif my_word in english_to_bulgarian_dictionary:
+    print("{} means {}".format (my_word, translation2))  
+else:
+        print("{} is not in this dictionary. Please try another word.") 
---
+++ control-flow/max-and-min-value.py
@@ -0,0 +1,10 @@
+#This program creates a list of numbers and print its largest and the smallest values.
+value = []
+
+# Print none if the list is empty.
+if value == []:
+        print([])
+
+# Print the largest and the smallest values of the list.
+else:
+    print(max(value), min(value))
