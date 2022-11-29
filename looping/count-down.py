82dc7b892325e3cbe135d4bdf444421a410320f2
hera-admin hera-admin+1-at-101171522119 <hera-admin hera-admin+1-at-101171522119>
2022-11-29 11:15:59 -0800

    This commit contain looping programing.

---
+++ looping/count-down.py
@@ -0,0 +1,7 @@
+count_down = 10
+print("\ncount_down from 10 to 0\n")
+while count_down >= 0:
+    print (count_down)
+    count_down = count_down - 1
+    if count_down <0:
+        print("\nCountdown is over!\n")
---
+++ looping/credentials-code.py
@@ -0,0 +1,28 @@
+#This program contains a code that first asks a user for a username and password.
+# Then, checks for the username and password validity. If correct it will grant access to user.
+# If not it will ask the user to enter their password or username 5 times. 
+# After 5 attempts,  the account would be locked.
+print('Enter your username and password to continue')
+count = 0
+
+# initializing by using "" or '' to assign a value string into it
+password = ""
+username = ""
+
+# looping will continue when wrong input for five times and ask again, and would locked the account.
+while username!='hdsm'  and password!='smile' and count < 5:
+    # collect user input from CLI separately.
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    if username=='hdsm' and password=='smile':
+     # if match, grand and break
+     print('You have access!')
+     break
+    
+    elif username!='hdsm' and password!='smile':
+        # if doesn't match, ask to try again
+        print('Enter your username or your password.')
+        # after 5 attempts account will be locked
+        count+=1
+        print('Account looked.')
