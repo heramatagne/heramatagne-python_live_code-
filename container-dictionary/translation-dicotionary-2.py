ae3c6fc0b9b43d9c91eb884d3e388c86aa34e647
hera-admin hera-admin+1-at-101171522119 <hera-admin hera-admin+1-at-101171522119>
2022-11-30 16:03:42 -0800

    This commit add a dictionary that allow the user to select the target language

---
+++ container-dictionary/translation-dicotionary-2.py
@@ -0,0 +1,56 @@
+#This program creates an English to French and English to Bulgarian translation dictionary.
+#This dictionary translate a  word from English to French.
+english_to_french_dictionary = {
+    "family" : "Famille",
+    "parent" : "Parent",
+    "father" : "Pere",
+    "mother" : "Mere",
+    "child" : "Enfant",
+    "son" : "Fils",
+    "daugther" : "Fille",
+    "brother" : "Frere",
+    "sister" : "Soeur",
+    "safou Plum" : "Prune/Safou"
+}
+
+#This dictionary translate a  word from English to bulgarian.
+english_to_bulgarian_dictionary = {
+    "family" : "семейство",
+    "parent" : "Родител",
+    "father" : "баща",
+    "mother" : "Майка",
+    "child" : "дете",
+    "son" : "син",
+    "daughter" : "Дъщеря",
+    "brother" : "Брат",
+    "sister" : "сестра",
+    "safou Plum" : "сафу слива"
+}
+
+#language selection
+language = input("Select the language of your choice. Enter FR for French or BG for Bulgarian:\n")
+
+if language.lower() == "fr":
+    word = input("Enter your word: ")
+    #English to French translation
+    if word in english_to_french_dictionary:
+        translation = english_to_french_dictionary.get(word)
+        print(f"{word} is {translation} in French.")
+    else:
+         print(f"{word} is not in this dictionary. Please try another word.")
+
+if language.lower() == "bg":
+    word = input("Enter your word: ")
+    #English to Bulgarian translation
+    if word in english_to_bulgarian_dictionary:
+        translation= english_to_bulgarian_dictionary.get(word)
+        print("{} is {} in Bulgarian.".format (word, translation))
+    else:
+        print(f"{word} is not in this dictionary. Please try another word.")
+
+# if my_word in english_to_french_dictionary:
+#     print("{} is {} in French.".format (my_word, translation))
+# elif my_word in english_to_bulgarian_dictionary:
+      
+# else:
+#         print(f"{my_word} is not in this dictionary. Please try another word.") 
